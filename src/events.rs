4c50c03b43ebf20ee83dd9453d2169747e8ef35c
anekos <anekos@snca.net>
2017-02-20 20:36:20 +0900

    Add `events` module

---
+++ src/events.rs
@@ -0,0 +1,43 @@
+
+use gtk::{Inhibit, main_quit};
+use gdk::EventKey;
+use std::sync::mpsc::Sender;
+
+use app::Operation;
+
+
+
+
+pub fn on_key_press(tx: Sender<Operation>, key: &EventKey) -> Inhibit {
+    use Operation::*;
+    use options::AppOptionName as opt;
+
+    if let Some(operation) = match key.as_ref().keyval {
+        104 | 102 => Some(First),
+        106 => Some(Next),
+        107 => Some(Previous),
+        108 => Some(Last),
+        113 => Some(Exit),
+        114 => Some(Refresh),
+        105 => Some(Toggle(opt::ShowText)),
+        key => if 48 <= key && key <= 57 {
+            Some(Count((key - 48) as u8))
+        } else {
+            Some(Key(key))
+        }
+    } {
+        tx.send(operation).unwrap();
+    }
+
+    Inhibit(false)
+}
+
+pub fn on_configure(tx: Sender<Operation>) -> bool {
+    tx.send(Operation::Refresh).unwrap();
+    false
+}
+
+pub fn on_delete() -> Inhibit {
+    main_quit();
+    Inhibit(false)
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ extern crate cairo;
 extern crate libc;
 #[macro_use] extern crate closet;
 
+mod events;
 mod http_cache;
 mod index_pointer;
 mod app;
@@ -39,8 +40,8 @@ fn main() {
     let (mut app, rx) = app::App::new(files, window.clone(), image.clone());
     let tx = app.tx.clone();
 
-    window.connect_key_press_event(clone_army!([tx] move |_, key| on_key_press(tx.clone(), key)));
-    window.connect_configure_event(clone_army!([tx] move |_, _| on_configure(tx.clone())));
+    window.connect_key_press_event(clone_army!([tx] move |_, key| events::on_key_press(tx.clone(), key)));
+    window.connect_configure_event(clone_army!([tx] move |_, _| events::on_configure(tx.clone())));
 
     stdin_reader(tx.clone());
 
@@ -58,37 +59,6 @@ fn main() {
         }
         sleep(Duration::from_millis(10));
     }
-}
-
-
-fn on_configure(tx: Sender<Operation>) -> bool {
-    tx.send(Operation::Refresh).unwrap();
-    false
-}
-
-
-fn on_key_press(tx: Sender<Operation>, key: &gdk::EventKey) -> gtk::Inhibit {
-    use Operation::*;
-    use options::AppOptionName as opt;
-
-    if let Some(operation) = match key.as_ref().keyval {
-        104 | 102 => Some(First),
-        106 => Some(Next),
-        107 => Some(Previous),
-        108 => Some(Last),
-        113 => Some(Exit),
-        114 => Some(Refresh),
-        105 => Some(Toggle(opt::ShowText)),
-        key => if 48 <= key && key <= 57 {
-            Some(Count((key - 48) as u8))
-        } else {
-            Some(Key(key))
-        }
-    } {
-        tx.send(operation).unwrap();
-    }
-
-    Inhibit(false)
 }
 
 
@@ -115,10 +85,7 @@ fn setup() -> (Window, Image) {
     window.set_border_width(0);
     window.set_position(gtk::WindowPosition::Center);
 
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
+    window.connect_delete_event(|_, _| events::on_delete());
 
     let image = Image::new_from_pixbuf(None);
     window.add(&image);

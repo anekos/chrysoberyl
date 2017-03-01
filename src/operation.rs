59d119758c860fcac75e13c2ee6b44e2bb17affb
anekos <anekos@snca.net>
2017-03-01 22:18:29 +0900

    Short hand and "@" prefix

--- src/operation.rs
+++ src/operation.rs
@@ -49,24 +49,24 @@ fn parse(s: &str) -> Operation {
         let args: Vec<String> = whole.collect();
 
         match name {
-            "push" => iter_let!(args => [path] {
+            "@push" => iter_let!(args => [path] {
                 return Push(path.to_owned())
             }),
-            "pushfile" => iter_let!(args => [path] {
+            "@pushfile" => iter_let!(args => [path] {
                 return PushFile(pathbuf(path))
             }),
-            "pushurl" => iter_let!(args => [path] {
+            "@pushurl" => iter_let!(args => [path] {
                 return PushURL(path.to_owned())
             }),
-            "next" => return Next,
-            "prev" | "previous" => return Previous,
-            "first" => return First,
-            "last" => return Last,
-            "refresh" => return Refresh,
-            "shuffle" => return Shuffle(true),
-            "sort" => return Sort,
-            "expand" => return Expand(args.get(0).map(|it| pathbuf(it))),
-            "expandrecursive" => return ExpandRecursive(args.get(0).map(|it| pathbuf(it))),
+            "@next" | "@n"               => return Next,
+            "@prev" | "@p" | "@previous" => return Previous,
+            "@first" | "@f"              => return First,
+            "@last" | "@l"               => return Last,
+            "@refresh" | "@r"            => return Refresh,
+            "@shuffle"                   => return Shuffle(true),
+            "@sort"                      => return Sort,
+            "@expand"                    => return Expand(args.get(0).map(|it| pathbuf(it))),
+            "@expandrecursive"           => return ExpandRecursive(args.get(0).map(|it| pathbuf(it))),
             _ => ()
         }
     }
@@ -80,46 +80,47 @@ fn test_parse() {
     use self::Operation::*;
 
     // Simple
-    assert_eq!(parse("First"), First);
-    assert_eq!(parse("Next"), Next);
-    assert_eq!(parse("Previous"), Previous);
-    assert_eq!(parse("Prev"), Previous);
-    assert_eq!(parse("Last"), Last);
-    assert_eq!(parse("shuffle"), Shuffle(true));
-    assert_eq!(parse("refresh"), Refresh);
-    assert_eq!(parse("sort"), Sort);
+    assert_eq!(parse("@First"), First);
+    assert_eq!(parse("@Next"), Next);
+    assert_eq!(parse("@Previous"), Previous);
+    assert_eq!(parse("@Prev"), Previous);
+    assert_eq!(parse("@Last"), Last);
+    assert_eq!(parse("@shuffle"), Shuffle(true));
+    assert_eq!(parse("@refresh"), Refresh);
+    assert_eq!(parse("@sort"), Sort);
 
     // 1 argument
-    assert_eq!(parse("push http://example.com/moge.jpg"), Push("http://example.com/moge.jpg".to_owned()));
-    assert_eq!(parse("pushfile /hoge/moge.jpg"), PushFile(pathbuf("/hoge/moge.jpg")));
-    assert_eq!(parse("pushurl http://example.com/moge.jpg"), PushURL("http://example.com/moge.jpg".to_owned()));
+    assert_eq!(parse("@push http://example.com/moge.jpg"), Push("http://example.com/moge.jpg".to_owned()));
+    assert_eq!(parse("@pushfile /hoge/moge.jpg"), PushFile(pathbuf("/hoge/moge.jpg")));
+    assert_eq!(parse("@pushurl http://example.com/moge.jpg"), PushURL("http://example.com/moge.jpg".to_owned()));
 
     // 1 optional argument
-    assert_eq!(parse("expand /foo/bar.txt"), Expand(Some(pathbuf("/foo/bar.txt"))));
-    assert_eq!(parse("expand"), Expand(None));
-    assert_eq!(parse("expandrecursive /foo/bar.txt"), ExpandRecursive(Some(pathbuf("/foo/bar.txt"))));
+    assert_eq!(parse("@expand /foo/bar.txt"), Expand(Some(pathbuf("/foo/bar.txt"))));
+    assert_eq!(parse("@expand"), Expand(None));
+    assert_eq!(parse("@expandrecursive /foo/bar.txt"), ExpandRecursive(Some(pathbuf("/foo/bar.txt"))));
     assert_eq!(parse("expandrecursive"), ExpandRecursive(None));
 
     // Invalid commands be PushFile
     assert_eq!(parse("Meow Meow"), PushFile(pathbuf("Meow Meow")));
+    assert_eq!(parse("expand /foo/bar.txt"), PushFile(pathbuf("expand /foo/bar.txt")));
 
     // Shell quotes
     assert_eq!(
-        parse(r#"Push "http://example.com/sample.png""#),
+        parse(r#"@Push "http://example.com/sample.png""#),
         Push("http://example.com/sample.png".to_owned()));
 
     // Shell quotes
     assert_eq!(
-        parse(r#"Push 'http://example.com/sample.png'"#),
+        parse(r#"@Push 'http://example.com/sample.png'"#),
         Push("http://example.com/sample.png".to_owned()));
 
     // Ignore leftover arguments
     assert_eq!(
-        parse(r#"Push "http://example.com/sample.png" CAT IS PRETTY"#),
+        parse(r#"@Push "http://example.com/sample.png" CAT IS PRETTY"#),
         Push("http://example.com/sample.png".to_owned()));
 
     // Ignore case
-    assert_eq!(parse("ShuFFle"), Shuffle(true));
+    assert_eq!(parse("@ShuFFle"), Shuffle(true));
 }
 
 fn pathbuf(s: &str) -> PathBuf {

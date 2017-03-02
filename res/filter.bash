6782518d49bda33cc61519e7d67e34b8b57a7eed
anekos <anekos@snca.net>
2017-03-02 20:43:44 +0900

    Fix filter script

--- res/filter.bash
+++ res/filter.bash
@@ -8,7 +8,11 @@ set -eC
 function chrysoberyl_filter_main () {
   while read -r line
   do
-    eval "$line"
+    if ! eval "$line"
+    then
+      echo "Error for: $line" 1>&2
+      continue
+    fi
 
     case "$event" in
       key)

1185a0ce6bae35fe97e829c24a1237b99d3fec19
anekos <anekos@snca.net>
2017-03-22 21:59:27 +0900

    Remove `single_char_pattern`

--- src/operation.rs
+++ src/operation.rs
@@ -70,7 +70,7 @@ impl Operation {
         let mut index = 0;
 
         for  arg in &args {
-            let sep = arg.find("=").unwrap_or(0);
+            let sep = arg.find('=').unwrap_or(0);
             let (key, value) = arg.split_at(sep);
             if key.is_empty() {
                 result.push((format!("arg{}", index), value.to_owned()));

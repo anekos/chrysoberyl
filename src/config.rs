d48b881970cc874735d61819fef2fc4692175712
anekos <anekos@snca.net>
2017-05-10 22:21:36 +0900

    Use `include_str!` to define default config source

--- src/config.rs
+++ src/config.rs
@@ -14,59 +14,7 @@ pub enum ConfigSource {
 }
 
 
-pub static DEFAULT_CONFIG: &'static str = "
-# scroll or move
-@map key h @scroll --size 0.8 left  @previous
-@map key j @scroll --size 0.8 down  @next
-@map key k @scroll --size 0.8 up    @previous
-@map key l @scroll --size 0.8 right @next
-
-# move
-@map key asciicircum @first
-@map key dollar      @last
-@map key g           @first --ignore-views
-@map key G           @last  --ignore-views
-
-# move (ignore views)
-@map key H @previous --ignore-views
-@map key J @next     --ignore-views
-@map key K @previous --ignore-views
-@map key L @next     --ignore-views
-
-# move (archive)
-@map key p @previous --archive
-@map key n @next     --archive
-
-# option
-@map key a @toggle auto-paging
-@map key i @toggle status-bar
-@map key f @cycle fit
-@map key r @toggle reverse
-@map key c @toggle center
-@map key v @views
-@map key V @views --rows
-
-# entries
-@map key z @shuffle
-@map key e @expand
-@map key E @expand --recursive
-@map key R @refresh
-@map key X @clear
-
-# misc
-@map key q @quit
-@map key q @quit
-@map key Escape @count
-@map key c @unclip
-
-# Mouse
-@map mouse 1 @scroll --size 0.8 down  @next
-@map mouse 2 @cherenkov --radius 0.02 --color random --spokes 50
-@map mouse 3 @scroll --size 0.8 up    @previous
-
-# Not default
-# @pre-fetch --page-size 4 --limit-of-items 100
-";
+pub static DEFAULT_CONFIG: &'static str = include_str!("../res/config/default.conf");
 
 
 pub fn load_config(tx: &Sender<Operation>, config_source: &ConfigSource) {

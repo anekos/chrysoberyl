95c57aed6277f9788d2ee94b5a79f0770281159b
anekos <anekos@snca.net>
2017-07-25 22:59:55 +0900

    Refine log for `script/open`

--- src/script.rs
+++ src/script.rs
@@ -6,29 +6,36 @@ use std::sync::mpsc::Sender;
 
 use config::DEFAULT_CONFIG;
 use operation::Operation;
+use utils::path_to_string;
 
 
 
 pub fn load(tx: &Sender<Operation>, source: &str) {
+    puts_event!("script/open");
+    load_from_str(tx, source);
+    puts_event!("script/close");
+}
+
+pub fn load_from_file(tx: &Sender<Operation>, file: &Path) {
+    puts_event!("script/open", "file" => path_to_string(&file));
+    let mut source = o!("");
+    match File::open(file).and_then(|mut file| file.read_to_string(&mut source)) {
+        Ok(_) => load_from_str(tx, &source),
+        Err(err) => puts_error!("at" => o!("on_load"), "reason" => s!(err)),
+    }
+    puts_event!("script/close", "file" => path_to_string(&file));
+}
+
+fn load_from_str(tx: &Sender<Operation>, source: &str) {
     let lines: Vec<&str> = source.lines().collect();
 
-    puts_event!("input/script/open");
     for line in lines {
         match Operation::parse(line) {
             Ok(op) =>
                 process(tx, op),
             Err(err) =>
-                puts_error!("at" => "input/script", "reason" => s!(err), "for" => o!(line)),
+                puts_error!("at" => "script/line", "reason" => s!(err), "for" => o!(line)),
         }
-    }
-    puts_event!("input/script/close");
-}
-
-pub fn load_from_file(tx: &Sender<Operation>, file: &Path) {
-    let mut source = o!("");
-    match File::open(file).and_then(|mut file| file.read_to_string(&mut source)) {
-        Ok(_) => load(tx, &source),
-        Err(err) => puts_error!("at" => o!("on_load"), "reason" => s!(err)),
     }
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -10,6 +10,10 @@ pub fn path_to_str<T: AsRef<Path>>(path: &T) -> &str {
     path.as_ref().to_str().unwrap()
 }
 
+pub fn path_to_string<T: AsRef<Path>>(path: &T) -> String {
+    path.as_ref().to_str().unwrap().to_owned()
+}
+
 pub fn s<T: Display>(x: &T) -> String {
     format!("{}", x)
 }

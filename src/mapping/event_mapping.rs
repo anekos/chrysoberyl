03332d18afab43251a9f34a7f9b8b7cd6f0b8bfa
anekos <anekos@snca.net>
2017-08-07 20:51:40 +0900

    Refine `EventMapping` structure

--- src/mapping/event_mapping.rs
+++ src/mapping/event_mapping.rs
@@ -6,11 +6,12 @@ use events::EventName;
 
 
 pub struct EventMapping {
-    pub table: HashMap<EventName, EventMappingEntry>,
+    pub table: HashMap<EventName, Vec<EventMappingEntry>>,
 }
 
 pub struct EventMappingEntry {
-    pub table: HashMap<Option<String>, Vec<Vec<String>>>
+    group: Option<String>,
+    operation: Vec<String>,
 }
 
 
@@ -21,49 +22,23 @@ impl EventMapping {
     }
 
     pub fn register(&mut self, event_name: EventName, group: Option<String>, operation: Vec<String>) {
-        if let Some(entry) = self.table.get_mut(&event_name) {
-            entry.register(group, operation);
-            return;
+        let entry = EventMappingEntry { group: group, operation: operation };
+
+        if let Some(entries) = self.table.get_mut(&event_name) {
+            return entries.push(entry);
         }
 
-        let mut entry = EventMappingEntry::new();
-        entry.register(group, operation);
-        self.table.insert(event_name, entry);
+        let entries = vec![entry];
+        self.table.insert(event_name, entries);
     }
 
     pub fn unregister(&mut self, event_name: &EventName, group: &Option<String>) {
-        if_let_some!(entry = self.table.get_mut(event_name), ());
-        entry.unregister(group);
+        if_let_some!(entries = self.table.get_mut(event_name), ());
+        entries.retain(|it| it.group == *group)
     }
 
     pub fn matched(&self, event_name: &EventName) -> Vec<Vec<String>> {
-        self.table.get(event_name).map(|it| it.entries()).unwrap_or_else(|| vec![])
-    }
-}
-
-impl EventMappingEntry {
-    pub fn new() -> Self {
-        EventMappingEntry { table: HashMap::new() }
-    }
-
-    pub fn register(&mut self, group: Option<String>, operation: Vec<String>) {
-        if let Some(entry) = self.table.get_mut(&group) {
-            entry.push(operation);
-            return;
-        }
-
-        self.table.insert(group, vec![operation]);
-    }
-
-    pub fn unregister(&mut self, group: &Option<String>) {
-        self.table.remove(group);
-    }
-
-    pub fn entries(&self) -> Vec<Vec<String>> {
-        let mut result = vec![];
-        for ops in self.table.values() {
-            result.extend_from_slice(ops)
-        }
-        result
+        if_let_some!(entries = self.table.get(event_name), vec![]);
+        entries.iter().map(|it| it.operation.clone()).collect()
     }
 }

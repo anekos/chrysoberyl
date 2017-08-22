2ffe4d0ee5b88d0dd0c4b7e91004bfb16b775c04
anekos <anekos@snca.net>
2017-08-22 22:15:37 +0900

    Write `~/.config/chrysoberyl/config.chry` if the file does not exist

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 
-use std::fs::File;
-use std::io::Read;
+use std::fs::{create_dir_all, File};
+use std::io::{Read, Write};
 
 use app_path;
 
@@ -12,8 +12,16 @@ pub static DEFAULT_CONFIG: &'static str = include_str!("static/default.chry");
 pub fn get_config_source() -> String {
     let file = app_path::config_file(None);
     let mut out = o!("");
-    match File::open(file).and_then(|mut file| file.read_to_string(&mut out)) {
+    match File::open(&file).and_then(|mut file| file.read_to_string(&mut out)) {
         Ok(_) => out,
-        Err(_) => o!(DEFAULT_CONFIG),
+        Err(_) => {
+            let dir = file.parent().unwrap();
+            let _ = create_dir_all(dir).and_then(|_| {
+                File::create(&file).and_then(|mut file| {
+                    file.write_all(DEFAULT_CONFIG.as_bytes())
+                })
+            });
+            o!(DEFAULT_CONFIG)
+        }
     }
 }

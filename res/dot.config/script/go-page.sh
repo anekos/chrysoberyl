e2acde049937b0cec326d4f7b819b92cda24c340
anekos <anekos@snca.net>
2017-08-25 22:22:20 +0900

    Add `go-page.sh` and the mapping

---
+++ res/dot.config/script/go-page.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -euC
+set -x
+
+# shellcheck disable=SC1090
+. "$(dirname "$0")/lib.sh"
+
+check_app
+
+# shellcheck disable=SC2012
+zenity --modal --attach "$WINDOWID" --scale --print-partial --text "$CHRY_PAGES pages" --value "$CHRY_PAGE" --min-value 1 --max-value "$CHRY_PAGES" | while read -r page
+do
+  printf '@first %q\n' "$page"
+done
+

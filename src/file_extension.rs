6de42ab5d712a1aa8697ffde3d6cab4f560ed717
anekos <anekos@snca.net>
2017-08-29 21:18:09 +0900

    Support more archive filename extensions

--- src/file_extension.rs
+++ src/file_extension.rs
@@ -11,7 +11,7 @@ pub fn get_entry_type_from_filename<T: AsRef<Path>>(path: &T) -> Option<EntryTyp
     let ext = ext.to_lowercase();
 
     match &*ext {
-        "zip" | "rar" | "tar.gz" | "lzh" | "lha" =>
+        "7z" | "ar" | "cab" | "cpio" | "iso9660" | "lha" | "lzh" | "mtree" | "pax" | "rar" | "shar" | "tar" | "xar" | "zip" =>
             Some(EntryType::Archive),
         "pdf" =>
             Some(EntryType::PDF),

4de940f362be60b58cb56cb4296c991a4d4d1053
anekos <anekos@snca.net>
2017-09-29 23:04:56 +0900

    Use `CONST` to read a env

--- src/text_format/mod.rs
+++ src/text_format/mod.rs
@@ -15,6 +15,7 @@ pub fn generate(source: &str) -> Result<String, Box<error::Error>> {
     let mruby = Mruby::new();
 
     let chry_mod = mruby.def_class("Chrysoberyl");
+    let object = mruby.def_class("Object");
 
     mruby.def_class_method(chry_mod.clone(), "env", mrfn!(|mruby, slf: Value, v: (&str)| {
         match env::var(v) {
@@ -23,18 +24,18 @@ pub fn generate(source: &str) -> Result<String, Box<error::Error>> {
         }
     }));
 
-    mruby.def_class_method(chry_mod.clone(), "env", mrfn!(|mruby, slf: Value, v: (&str), def: (&str)| {
+    mruby.def_class_method(chry_mod, "env", mrfn!(|mruby, slf: Value, v: (&str), def: (&str)| {
         match env::var(v) {
             Ok(s) => mruby.string(&s),
             Err(_) => mruby.string(def),
         }
     }));
 
-    mruby.def_class_method(chry_mod, "method_missing", mrfn!(|mruby, slf: Value, name: Value| {
+    mruby.def_class_method(object, "const_missing", mrfn!(|mruby, slf: Value, name: Value| {
         fetch_var(&mruby, &name).unwrap_or_else(|_| mruby.nil())
     }));
 
-    let source = format!("Chrysoberyl.instance_eval {{ {} }}", source);
+    let source = format!("Chrysoberyl.instance_eval {{ {} }} .to_s", source);
     let result: Value = mruby.run(&source)?;
     let result = result.to_str()?;
     Ok(o!(result))
@@ -44,6 +45,6 @@ pub fn generate(source: &str) -> Result<String, Box<error::Error>> {
 fn fetch_var(mruby: &Rc<RefCell<Mruby>>, name: &Value) -> Result<Value, Box<error::Error>> {
     let name = name.to_str()?;
     let name = constant::env_name(name);
-    let value= env::var(name)?;
+    let value = env::var(name)?;
     Ok(mruby.string(&value))
 }

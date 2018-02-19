8a8739be27ead8cdeff8674f5503565186c242e6
anekos <anekos@snca.net>
2018-02-19 22:30:04 +0900

    Thread safe lazy

--- src/lazy.rs
+++ src/lazy.rs
@@ -1,11 +1,12 @@
 
+use std::sync::{Arc, Mutex};
 use std::cell::RefCell;
 
 
 
 #[derive(Clone)]
 pub struct Lazy<T> {
-    inner: RefCell<Inner<T>>,
+    inner: Arc<Mutex<RefCell<Inner<T>>>>,
 }
 
 #[derive(Clone)]
@@ -17,12 +18,15 @@ enum Inner<T> {
 
 impl<T> Lazy<T> {
     pub fn new() -> Self {
-        Lazy { inner: RefCell::new(Inner::Initial) }
+        Lazy { inner: Arc::new(Mutex::new(RefCell::new(Inner::Initial))) }
     }
 
     pub fn get<F>(&self, ctor: F) -> &T where F: FnOnce() -> T {
         self.evaluate(ctor);
-        let inner = unsafe { self.inner.as_ptr().as_ref().unwrap() };
+
+        let inner = self.inner.lock().unwrap();
+
+        let inner = unsafe { inner.as_ptr().as_ref().unwrap() };
         match *inner {
             Inner::Evaludated(ref v) => v,
             _ => panic!("WTF"),
@@ -30,11 +34,13 @@ impl<T> Lazy<T> {
     }
 
     pub fn evaluate<F>(&self, ctor: F) where F: FnOnce() -> T {
-        if let Inner::Evaludated(_) = *self.inner.borrow() {
+        let inner = self.inner.lock().unwrap();
+
+        if let Inner::Evaludated(_) = *inner.borrow() {
             return;
         }
 
-        let mut inner = self.inner.borrow_mut();
+        let mut inner = inner.borrow_mut();
         *inner = Inner::Evaludated(ctor());
     }
 }

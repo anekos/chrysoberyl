bd6a3a156016d65b61ea86f3666a841b0ac872bb
anekos <anekos@snca.net>
2018-06-24 00:17:48 +0900

    I can't differentiate between vertical and horizontal

--- src/gtk_utils.rs
+++ src/gtk_utils.rs
@@ -27,12 +27,12 @@ pub fn context_flip(context: &Context, size: &Size, horizontal: bool, vertical: 
     }
     let (mut xx, yx, xy, mut yy, mut x0, mut y0)  = (1.0, 0.0, 0.0, 1.0, 0.0, 0.0);
     if horizontal {
-        xx = -1.0;
-        x0 = f64!(size.width);
-    }
-    if vertical {
         yy = -1.0;
         y0 = f64!(size.height);
+    }
+    if vertical {
+        xx = -1.0;
+        x0 = f64!(size.width);
     }
     context.transform(Matrix::new(xx, yx, xy, yy, x0, y0));
 }

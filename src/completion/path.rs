20cd404cd3143633efc6ebd21a51e6642ef4467e
anekos <anekos@snca.net>
2018-08-22 23:24:19 +0900

    Improve command line completer

--- src/completion/path.rs
+++ src/completion/path.rs
@@ -8,21 +8,21 @@ use shellexpand_wrapper as sh;
 pub fn get_candidates(path: &str, directory_only: bool, prefix: &str, result: &mut Vec<String>) {
     let path = &path[min!(prefix.len(), path.len())..];
     let path = sh::expand_to_pathbuf(path);
+
+    if_let_ok!(cwd = current_dir(), |_| ());
+
     let entries = if path.iter().count() == 0 {
-        if_let_ok!(dir = current_dir(), |_| ());
-        dir.read_dir()
+        cwd.read_dir()
     } else if path.is_dir() {
         path.read_dir()
     } else if let Some(dir) = path.parent() {
         if dir.is_dir() {
             dir.read_dir()
         } else {
-            if_let_ok!(dir = current_dir(), |_| ());
-            dir.read_dir()
+            cwd.read_dir()
         }
     } else {
-        if_let_ok!(dir = current_dir(), |_| ());
-        dir.read_dir()
+        cwd.read_dir()
     };
 
     if_let_ok!(entries = entries, |_| ());
@@ -38,7 +38,11 @@ pub fn get_candidates(path: &str, directory_only: bool, prefix: &str, result: &m
                     }
                 }
             }
-            if let Some(path) = entry.path().to_str() {
+            if let Ok(stripped) = entry.path().strip_prefix(&cwd) {
+                if let Some(path) = stripped.to_str() {
+                    result.push(format!("{}{}", prefix, path));
+                }
+            } else if let Some(path) = entry.path().to_str() {
                 if path.starts_with(home) {
                     result.push(format!("{}~{}", prefix, &path[home.len()..]));
                 } else {

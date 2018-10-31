5dd4f16fef1310a9c9b592cce06510cac33d92f5
anekos <anekos@snca.net>
2018-11-01 00:24:42 +0900

    Add `cycle_uint` macro

--- src/util/num.rs
+++ src/util/num.rs
@@ -11,3 +11,34 @@ pub fn feq(x: f64, y: f64, error: f64) -> bool {
 pub fn range_contains<T: PartialOrd>(range: &Range<T>, index: &T) -> bool {
     range.start <= *index && *index <= range.end
 }
+
+macro_rules! cycle_uint {
+    ( $type:ty, $reverse:expr, $n:expr, $target:ident ) => {
+        {
+            let reverse = $reverse;
+            let n = $n as $type;
+            if reverse {
+                $target.wrapping_sub(n)
+            } else {
+                $target.wrapping_add(n)
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]#[test]
+fn test_cycle_uint() {
+    fn cycle(x: u8, reverse: bool, n: usize) -> u8 {
+        cycle_uint!(u8, reverse, n, x)
+    }
+
+    assert_eq!(256usize as u8, 0);
+    assert_eq!(257usize as u8, 1);
+
+    assert_eq!(cycle(1, false, 1), 2);
+    assert_eq!(cycle(255, false, 1), 0);
+    assert_eq!(cycle(255, false, 2), 1);
+    assert_eq!(cycle(0, false, 255), 255);
+    assert_eq!(cycle(0, false, 256), 0);
+}
